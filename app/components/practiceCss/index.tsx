e83039f2a1d47f298850de2772a4a7c9d76cbb2f
main-uddin <mdmain74@gmail.com>
2026-02-07 03:05:42 +0600

    practice grid

--- app/components/practiceCss/index.tsx
+++ app/components/practiceCss/index.tsx
@@ -48,6 +48,21 @@ const PracticeCss = () => {
         <img src="https://picsum.photos/300/130" alt="" />
         <img src="https://picsum.photos/300/200" alt="" />
       </div>
+
+      <div className="py-16">Practice grid elements</div>
+
+      <div className="grid-container">
+        <div className="item1">1</div>
+        <div className="item2">2</div>
+        <div className="item3">3</div>
+        <div className="item4">4</div>
+        <div className="item5">5</div>
+        <div className="item6">6</div>
+        <div className="item7">7</div>
+        <div className="item8">8</div>
+        <div className="item9">9</div>
+        <div className="item10">10</div>
+      </div>
     </div>
   );
 };
